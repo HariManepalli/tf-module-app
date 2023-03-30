41b13cd94721b9d81d9ce7228b9228f282ceacd5
Hariprasad <hari20185@gmail.com>
2023-03-30 08:25:09 +0530

    tf-module-app-changes

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "aws_launch_template" "main" {
 
     tags = merge(
       var.tags,
-      { Name = "${var.component}-${var.env}", Monitor = "yes" }
+      { Name = "${var.component}-${var.env}" }
     )
   }
 
