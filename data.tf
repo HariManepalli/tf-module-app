978169d64a0402faae2065a6967b7d63faf2f28e
Hariprasad <hari20185@gmail.com>
2023-04-12 10:17:38 +0530

    tf-module-app-changes

--- data.tf
+++ data.tf
@@ -1,14 +1,14 @@
-/*data "aws_ami" "ami" {
+data "aws_ami" "ami" {
   most_recent = true
   name_regex  = "devops-practice-with-ansible"
   owners      = ["self"]
-}*/
+}
 
-data "aws_ami" "ami" {
+/*data "aws_ami" "ami" {
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
   owners      = ["973714476881"]
-}
+}*/
 
 data "aws_caller_identity" "account" {}
 
